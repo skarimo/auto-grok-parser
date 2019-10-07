fb5c15b954a86b5a31b9e0564a3fb1e3b8915828
skarimo <karimovj@gmail.com>
2019-10-06 22:44:40 -0400

    some refactoring and message udpates

--- app.py
+++ app.py
@@ -3,30 +3,35 @@ import os
 import re
 from parser import Parser
 
-def main_app():
+def app():
     global parser_class
-    global raw_log_display
 
     click.clear()
-    click.secho('WELCOME', blink=True, bold=True)
+    click.secho('WELCOME TO CLI GROK PARSER HELPER', blink=True, bold=True)
 
     raw_log = click.prompt('Please enter a raw log line', type=str)
-    raw_log_display = raw_log
     parser_class = Parser(raw_log)
 
-    click.clear()
+    main_prompt_loop()
 
-    while True:
+    end_message()
+
+def main_prompt_loop():
+    global parser_class
+
+    while len(parser_class.raw_log_display) != 0:
+        click.clear()
         print_raw_log_and_current_rule()
         attribute_input = click.prompt('Copy paste the first/next attribute value', type=str)
         if attribute_input.lower() == 'end':
             break
         else:
             attribute_name = click.prompt('Name the attribute: ' + attribute_input, type=str)
-            # raw_log_display = re.sub(attribute_input,'',raw_log_display,count=1)
-            raw_log_display = raw_log_display.replace(attribute_input, '', 1)
-            parser_class.match_string(attribute_name, attribute_input)
+            parser_class.handle_attribute(attribute_name, attribute_input)
             click.clear()
+
+        if len(parser_class.raw_log_display) == 0:
+            break
 
         print_raw_log_and_current_rule()
         divider_input = click.prompt('Copy paste the divider | type [n] if no divider', type=str)
@@ -35,25 +40,29 @@ def main_app():
         elif divider_input.lower() == 'n':
             pass
         else:
-            raw_log_display = raw_log_display.replace(divider_input, '', 1)
-            parser_class.append_divider(divider_input)
+            parser_class.handle_divider(divider_input)
             click.clear()
-            
-    click.clear()
-    click.echo('Created rule:')
-    click.secho('new_rule ' + ''.join(parser_class.grok_parser), bg='red', fg='white')
 
 def print_raw_log_and_current_rule():
     global parser_class
-    global raw_log_display
+
     click.secho('new_rule ' + ''.join(parser_class.grok_parser), bg='red', fg='white')
     click.echo('')
+    click.echo('')
+    click.echo('')
     click.echo('Raw Log:')
-    click.secho(raw_log_display, bg='blue', fg='white')
+    click.secho(parser_class.raw_log_display, bg='blue', fg='white')
     click.secho('TYPE "end" if finished', bg='white', fg='black')
 
+def end_message():
+    global parser_class
 
-    pass
+    click.clear()
+    click.echo('Inputted log:')
+    click.secho(parser_class.raw_log, bg='blue', fg='white')
+    click.echo('')
+    click.echo('Created rule:')
+    click.secho('new_rule ' + ''.join(parser_class.grok_parser), bg='red', fg='white', bold=True)
 
 if __name__ == '__main__':
-    main_app()
+    app()
